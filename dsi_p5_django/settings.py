609adaeb1a71f4e53cc4e0e1f6350fc3c5cdf809
Jonay Suárez Armas <alu0100600674@ull.edu.es>
2014-03-17 11:26:40 +0000

    Modificado el settings.py

--- dsi_p5_django/settings.py
+++ dsi_p5_django/settings.py
@@ -11,13 +11,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'learning_lettuce.db',
     }
 }
 
@@ -124,6 +119,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'blog',
 )
 
 # A sample logging configuration. The only tangible logging
